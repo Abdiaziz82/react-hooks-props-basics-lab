3cd543fd39add88c30d31b09d84bcb61df916e06
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-06-27 11:09:55 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // Opening curly brace for function body
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {props.bio ? (
+        <p>{props.bio}</p>
+      ) : (
+        null
+      )}
+      
+      {/* Image element */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      {/* Links component */}
+      <Links
+        github="https://github.com/liza"
+        linkedin="https://www.linkedin.com/in/liza/"
+      />
     </div>
   );
+ 
 }
 
 export default About;
+
